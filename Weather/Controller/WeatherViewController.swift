0f3cadd71f0be0b6b030d99929a8f22dbe13f139
DZCSE8O <miroslav.hnat@celverlance.com>
2020-12-22 10:12:22 +0100

    initial projects and set UI

---
+++ Weather/Controller/WeatherViewController.swift
@@ -0,0 +1,49 @@
+//
+//  WeatherViewController.swift
+//  Weather
+//
+//  Created by Miroslav Hnát on 26.11.2020.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        searchTextField.delegate = self
+    }
+    
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if searchTextField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something..."
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        //Use searchTextField.text to get the weather for that city.
+        
+        searchTextField.text = ""
+    }
+}
+
