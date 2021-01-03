b7b21fce13e14f84b31a1544b867ae38b4cf3b53
MiraH <hnatmira@gmail.com>
2021-01-03 19:34:46 +0100

    API call for City name and current teperature for city

---
+++ Weather/Model/WeatherData.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherData.swift
+//  Weather
+//
+//  Created by Miroslav Hnat  on 01.01.2021.
+//
+
+import Foundation
---
+++ Weather/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  Weather
+//
+//  Created by Miroslav Hnat  on 24.12.2020.
+//
+
+import Foundation
